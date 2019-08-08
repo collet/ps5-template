2e921fd53484f092073f47aac30ec141798c765e
Philippe Collet <Philippe.Collet@unice.fr>
2019-08-08 17:13:13 +0200

    more explicit exemplary test (setup and var)

--- src/test/java/fr/unice/polytech/startingpoint/MainTest.java
+++ src/test/java/fr/unice/polytech/startingpoint/MainTest.java
@@ -1,13 +1,21 @@
 package fr.unice.polytech.startingpoint;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    String varToBeInitInSetup;
+
+    @BeforeEach
+    void setUp() {
+        varToBeInitInSetup = "Hello World!";
+    }
+
     @Test
     void helloTest() {
-        assertEquals("Hello World!", Main.hello());
+        assertEquals(varToBeInitInSetup, Main.hello());
     }
 }
